79ebcbcf185e7baf4a13f888a12ae8144fad4092
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-19 01:17:18 +0800

    Create multiples_of_3_or_5.c

---
+++ C/multiples_of_3_or_5.c
@@ -0,0 +1,14 @@
+int solution(int number) {
+  
+		if (number < 0)
+      return 0;
+    
+    int sum = 0;
+  
+    for (int i = 3; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0)
+          sum += i;
+    }
+    
+  return sum; 
+}
