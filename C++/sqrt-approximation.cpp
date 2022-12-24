1db459d72ab35a42be613a3fe5b1c778b514a56f
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-24 18:37:28 +0800

    Create sqrt-approximation.cpp

---
+++ C++/sqrt-approximation.cpp
@@ -0,0 +1,17 @@
+std::vector<int> sqrtApproximation(int number) {
+  std::vector<int> sqrt;
+  
+  int i, prod = 0;
+  for (i = 0; prod <= number; i++) {
+    prod = i * i;
+    if (prod == number) {
+      sqrt.push_back(i);
+      return sqrt;
+    }
+  }
+  
+  sqrt.push_back(i-2);
+  sqrt.push_back(i-1);
+  
+  return sqrt;
+}
