d4ec7aaf682b57280b9a936d9d211bb709eee505
kulaizki <91787757+kulaizki@users.noreply.github.com>
2023-01-01 00:19:41 +0800

    Update scramblies.cpp

--- C++/scramblies.cpp
+++ C++/scramblies.cpp
@@ -9,7 +9,7 @@ bool scramble(const string& s1, const string & s2) {
 
     for (int i: s1) h1[i]++;
     for (int i: s2) h2[i]++;
-    for (auto& [key, value] : h2) {
+    for (const auto& [key, value] : h2) {
         if (h1[key] < value) { return false; };
     }
     return true;
