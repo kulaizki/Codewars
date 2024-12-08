153c28822e4eebd2da95036e2c589fddcb79f258
kulaizki <kulaizke@gmail.com>
2024-12-08 09:09:36 +0800

    feat: add rot13

---
+++ Python/rot13.py
@@ -0,0 +1,10 @@
+def rot13(message):
+    ans = ""
+    for c in message:
+        if c.isalpha():
+            x = 65 if c.isupper() else 97
+            ans += chr((ord(c) - x + 13) % 26 + x)
+        else:
+            ans += c
+    return ans
+    pass
