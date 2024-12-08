c52f0e4f0f55051aebd4c3bd95fb7f2558c6acb8
kulaizki <kulaizke@gmail.com>
2024-12-08 09:09:08 +0800

    feat: add valid-braces

---
+++ Python/valid-braces.py
@@ -0,0 +1,13 @@
+def valid_braces(string):
+    s = []
+    
+    mp = {')': '(', ']': '[', '}': '{'}
+    
+    for c in string:
+        if c in mp.values():
+            s.append(c)
+        elif c in mp:
+            if not s or s.pop() != mp[c]:
+                return False
+    
+    return len(s) == 0
