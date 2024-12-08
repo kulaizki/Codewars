d87b3ed5c8ad9c28301410dfe4c9a46207fdbbfd
kulaizki <kulaizke@gmail.com>
2024-12-08 09:12:45 +0800

    refactor: merge condition into one

--- Python/valid-braces.py
+++ Python/valid-braces.py
@@ -1,13 +1,11 @@
 def valid_braces(string):
     s = []
-    
     mp = {')': '(', ']': '[', '}': '{'}
     
     for c in string:
         if c in mp.values():
             s.append(c)
-        elif c in mp:
-            if not s or s.pop() != mp[c]:
-                return False
+        elif c in mp and (not s or s.pop() != mp[c]):
+            return False
     
     return len(s) == 0
